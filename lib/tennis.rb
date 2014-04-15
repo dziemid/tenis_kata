4f8518a817bf77228e29bcbfcb60e3d5084f0655
jgaylard <jim.gaylard@rea-group.com>
2014-04-15 17:49:25 +1000

    Refactored

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -1,8 +1,15 @@
 class Tennis
   attr_reader :player_one_score
 
+  SCORING = {
+    0 => 0,
+    1 => 15,
+    2 => 30,
+    3 => 40
+  }
+
   def initialize
-    @player_one_score = 0
+    @player_one_points = 0
   end
 
   def player_two_score
@@ -10,8 +17,11 @@ class Tennis
   end
 
   def player_one_scores_point
-    @player_one_score += 15
-    @player_one_score = [@player_one_score, 40].min
+    @player_one_points += 1
+  end
+
+  def player_one_score
+    SCORING[@player_one_points]
   end
 
 end
