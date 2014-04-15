0a065cf1af0f9026acf19eddac82cb8d0dba09c2
Ilya Paripsa <iparipsa@thoughtworks.com>
2014-04-15 18:15:22 +1000

    add second player, can score up to duce. Present: Greg, Jim

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -10,14 +10,19 @@ class Tennis
 
   def initialize
     @player_one_points = 0
+    @player_two_points = 0
   end
 
   def player_two_score
-    0
+    SCORING[@player_two_points]
   end
 
   def player_one_scores_point
     @player_one_points += 1
+  end
+
+  def player_two_scores_point
+    @player_two_points += 1
   end
 
   def player_one_score
