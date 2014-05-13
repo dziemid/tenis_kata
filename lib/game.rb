8784a81272de2b3824880728fbc8a44cfc68e8f9
Greg Dziemidowicz <greg@cogent.co>
2014-05-13 17:37:18 +1000

    refactor

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,28 @@
 class Game
+
   def scores events
     player1score = score_for(events, 1)
     player2score = score_for(events, 2)
-    if  player1score > 45 
-      return ["won","lost"]     
-    elsif player2score > 45
-      return ["lost","won"]
-    end
-    [player1score, player2score]
+    display_score(player1score, player2score)
   end
 
   private
+
+  def winner_index(scores)
+    scores.each_with_index do |score, i|
+      return i if score > 45
+    end
+    nil
+  end
+
+  def display_score(player1score, player2score)
+    scores = [player1score, player2score]
+    if winner_index = winner_index(scores)
+      scores[winner_index] = "won"
+      scores[winner_index-1] = "lost"
+    end
+    scores
+  end
 
   def score_for events, player
     score = events.select {|i| i == player }.count*15
